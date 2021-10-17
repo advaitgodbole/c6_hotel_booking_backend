cf08c378bd01bd4892a18e1528297afb885ce52f
Advait Godbole <advaitgodbole9@gmail.com>
2021-10-17 23:34:12 +0200

    Eureka discovery server config complete

--- Sweet-home/booking-service/src/main/java/com/sweethome/bookingservice/BookingServiceApplication.java
+++ Sweet-home/booking-service/src/main/java/com/sweethome/bookingservice/BookingServiceApplication.java
@@ -2,12 +2,32 @@ package com.sweethome.bookingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookingServiceApplication.class, args);
 	}
 
+	public static ArrayList<String> getRandomNumbers(int count){
+		Random rand = new Random();
+		int upperBound = 100;
+		ArrayList<String>numberList = new ArrayList<String>();                 
+ 
+        for (int i=0; i<count; i++){
+			numberList.add(
+				String.valueOf(
+					rand.nextInt(upperBound)
+				)
+			);                 
+		}                 
+ 
+		return numberList;                 
+	} 
 }
